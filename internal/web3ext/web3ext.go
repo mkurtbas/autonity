5a39ad00df5ddca801a3610a401eb948f329f829
Miya Chen <miyatlchen@gmail.com>
2018-07-03 18:50:38 +0800

    eth, internal: add debug api to query receipts with given block hash

--- internal/web3ext/web3ext.go
+++ internal/web3ext/web3ext.go
@@ -460,6 +460,12 @@ web3._extend({
 			params: 1,
 			inputFormatter: [null],
 		}),
+		new web3._extend.Method({
+			name: 'getBlockReceipts',
+			call: 'debug_getBlockReceipts',
+			params: 1,
+			inputFormatter: [null],
+		}),
 	],
 	properties: []
 });
