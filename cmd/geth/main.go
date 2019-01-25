8bbd4b891cf8612f10bd5eaf889c146eaf8c1a9b
Edwin <edwin@am.is>
2019-01-25 14:26:35 +0800

    *: add broadcast local txs feature

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -75,6 +75,7 @@ var (
 		utils.TxPoolNoLocalsFlag,
 		utils.TxPoolJournalFlag,
 		utils.TxPoolRejournalFlag,
+		utils.TxPoolBroadcastPendingLocalTxFlag,
 		utils.TxPoolPriceLimitFlag,
 		utils.TxPoolPriceBumpFlag,
 		utils.TxPoolAccountSlotsFlag,
